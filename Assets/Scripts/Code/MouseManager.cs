dbff3dd0eda830abb5b1238c958c7a48499e35d7
Marc David <5david@informatik.uni-hamburg.de>
2020-05-06 17:49:46 +0200

    Implemented Basic Mouse Movement. Borders are still missing.

---
+++ Assets/Scripts/Code/MouseManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    int _boundary = 50;
+    float _edgeMovementSpeed = 10f;
+    float _dragSpeed = 1.4f;
+
+    float _scrollUpperLimit = 100;
+    float _scrollLowerLimit = 20;
+
+    float _zoomIncrement = 0.6f;
+
+    int _screenHeight;
+    int _screenWidth;
+
+
+    private Vector3 dragOrigin;
+
+    void Start()
+    {
+        _screenHeight = Screen.height;
+        _screenWidth = Screen.width;
+    }
+
+    void Update()
+    {
+        panCamera();
+        zoomScroll();
+
+    }
+
+    //TODO deal with map boundaries
+    void panCamera()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(1)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.y * _dragSpeed, 0, pos.x * _dragSpeed);
+
+        GetComponent<Transform>().position = new Vector3(transform.position.x + move.x, transform.position.y + move.y, transform.position.z - move.z);
+
+    }
+
+
+    void zoomScroll()
+    {
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) // Zoom in
+        {
+            float newYPos = transform.position.y - _zoomIncrement;
+
+            if (newYPos < _scrollLowerLimit)
+            {
+                return;
+            }
+
+            GetComponent<Transform>().position = new Vector3(transform.position.x, newYPos, transform.position.z);
+            transform.Rotate(-0.1f, 0, 0);
+        }
+
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0) // Zoom out
+        {
+
+            float newYPos = transform.position.y + _zoomIncrement;
+            if (newYPos > _scrollUpperLimit)
+            {
+                return;
+            }
+
+            GetComponent<Transform>().position = new Vector3(transform.position.x, newYPos, transform.position.z);
+            transform.Rotate(0.1f, 0, 0);
+
+        }
+
+    }
+
+
+    void edgeMovement()
+    {
+
+        if (Input.mousePosition.x > _screenWidth - _boundary)
+        {
+            Debug.Log("Hello World reached the boundary right");
+
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (_edgeMovementSpeed * Time.deltaTime));
+
+        }
+
+        if (Input.mousePosition.x < 0 + _boundary)
+        {
+            Debug.Log("Hello World reached the boundary left");
+        }
+
+
+
+        if (Input.mousePosition.y > _screenHeight - _boundary)
+        {
+            Debug.Log("Hello World reached the boundary upper");
+        }
+
+        if (Input.mousePosition.y < 0 + _boundary)
+        {
+            Debug.Log("Hello World reached the boundary lower");
+
+        }
+
+
+    }
+
+}
--- Assets/Scripts/Code/TileManager.cs
+++ Assets/Scripts/Code/TileManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseManager : MonoBehaviour
+public class TileManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
