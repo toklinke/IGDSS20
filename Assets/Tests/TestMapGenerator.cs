a786dcf5172774d238a1051caf31c58a46c42091
toklinke <43620409+toklinke@users.noreply.github.com>
2020-06-07 00:06:17 +0200

    rename IMapGenerator.GenerateMapFromHeightMap() (NFC)
    - make name more abstract

--- Assets/Tests/TestMapGenerator.cs
+++ Assets/Tests/TestMapGenerator.cs
@@ -130,7 +130,7 @@ namespace Tests
             }
         );
 
-        // Test that GenerateMapFromHeightMap() generates
+        // Test that GenerateMap() generates
         // the correct map
         [Test, TestCaseSource("GenerateMapFromHeightMapTestCases")]
         public void TestGenerateMapFromHeightMap(
@@ -145,7 +145,7 @@ namespace Tests
                 width: testCase.HeightMapWidth,
                 height: testCase.HeightMapHeight
             );
-            Map map = mapGenerator.GenerateMapFromHeightMap(heightMap);
+            Map map = mapGenerator.GenerateMap(heightMap);
 
             if (testCase.ExpectedWidth.HasValue)
                 Assert.That(map.Width, Is.EqualTo(testCase.ExpectedWidth));
