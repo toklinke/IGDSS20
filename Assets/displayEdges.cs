a70f29ad674dfef283561f86900f869e3f5086fd
Marc David <5david@informatik.uni-hamburg.de>
2020-07-06 22:21:41 +0200

    Fixed Post Processing

---
+++ Assets/displayEdges.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayEdges : MonoBehaviour
+{
+    public bool displayEdgeUP = false;
+
+    public bool displayEdgeUpperLeft = false;
+    public bool displayEdgeUpperRight = false;
+
+    public bool displayEdgeLowerLeft = false;
+    public bool displayEdgeLowerRight = false;
+
+    public bool displayEdgeDown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
